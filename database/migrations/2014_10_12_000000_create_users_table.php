f803a02fa74a1a9abf20ba36c2add542178a4180
restuadkh <restuadimas11@yahoo.com>
2023-11-16 13:35:20 +0700

    update kecil

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username'); 
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password'); 
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->timestamp('register_date')->useCurrent();
+            $table->boolean('is_admin')->default(false);
+            $table->timestamp('last_login')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
